ca65bcc2df07e66524f91af79b0018f20537f4eb
gioele134 <gioeleleanza@gmail.com>
2025-04-02 18:13:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -87,5 +87,8 @@ def aggiorna_disponibilita():
 
 # ------------------- Avvio -------------------
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
